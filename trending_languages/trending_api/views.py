ad945dbcf0f50629509a9f410ed000e279937057
Alaa5090 <alaaabdraboo297@gmail.com>
2021-05-12 02:57:10 +0200

    add response object , add function to get repos for language

--- trending_languages/trending_api/views.py
+++ trending_languages/trending_api/views.py
@@ -13,6 +13,39 @@ def trending_lang(request):
     url = "https://api.github.com/search/repositories?q=created:>{0}&sort=stars&order=desc&page=1&per_page=100".format(last_month)
     data=requests.get(url).json()
 
-    set_of_lang = {element['language'] for element in data['items'] if element['language'] != None}
-    
-    return HttpResponse(json.dumps(data), content_type="application/json")
+    languages = {element['language'] for element in data['items'] if element['language'] != None}
+    response_object = {
+		"languages_num" : len(languages),
+		"incomplete_results": data['incomplete_results'],
+		"languages" : []
+	}
+    for language in languages:
+        language_repos = get_lagugage_repos(language, data['items'])
+        
+        response_object['languages'].append({
+			"name" : language,
+			"repositories_count" : len(language_repos),
+			"repositories" : language_repos
+		})
+    return HttpResponse(json.dumps(response_object), content_type="application/json")
+
+
+def	get_lagugage_repos(language, repositories):
+	'''
+		Parameters:
+			repositories (string): repositories list
+			language (string): languages name
+		Return:
+			list of repos for specific language
+	'''
+	repos = []
+	for repo in repositories:
+		if repo['language'] == language:
+			# construct repo with information we need
+			repo_info = {
+				"repo_name" : repo['name'],
+				"repo_url" : repo['html_url'],
+				"stars_count" : repo['stargazers_count']
+			}
+			repos.append(repo_info)
+	return repos
